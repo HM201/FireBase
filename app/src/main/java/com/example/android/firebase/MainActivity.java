c54df4ef2a852f9d9fc4621c03ad63f545272fe2
Hisham <hishammahfouz201@gmail.com>
2019-04-06 16:23:53 +0200

    added storage

--- app/src/main/java/com/example/android/firebase/MainActivity.java
+++ app/src/main/java/com/example/android/firebase/MainActivity.java
@@ -1,23 +1,74 @@
 package com.example.android.firebase;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
     private String TAG = "Firebase";
+    private StorageReference storageRef;
+    private DatabaseReference myRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        storageRef = FirebaseStorage.getInstance().getReference();
+
+        Uri file = Uri.fromFile(new File("/storage/emulated/0/Pictures/Discord/bosy.jpg"));
+
+        final StorageReference ref = storageRef.child("images/"+file.getLastPathSegment());
+        UploadTask uploadTask = ref.putFile(file);
+
+        Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+            @Override
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                if (!task.isSuccessful()) {
+                    throw task.getException();
+                }
+
+                // Continue with the task to get the download URL
+                return ref.getDownloadUrl();
+            }
+        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                if (task.isSuccessful()) {
+                    Uri downloadUri = task.getResult();
+                    uploadUriToDatabase(downloadUri.toString());
+
+                } else {
+                    // Handle failures
+                    // ...
+                }
+            }
+        });
+
+
+
+
+
+
+
 
 
 
@@ -56,9 +107,42 @@ public class MainActivity extends AppCompatActivity {
                 });
 
 
+    }
+
+    public void uploadFile(View view) {
+        String downloadUrl;
+        Uri file = Uri.fromFile(new File("/storage/emulated/0/Pictures/Discord/bosy.jpg"));
+        final StorageReference ref = storageRef.child("images/"+file.getLastPathSegment());
+        UploadTask uploadTask = ref.putFile(file);
+        Task<Uri> downloadUri = ref.getDownloadUrl();
+
+        uploadUriToDatabase(downloadUri.getResult().toString());
+
+        // Register observers to listen for when the download is done or if it fails
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                // ...
+            }
+        });
+
 
     }
 
+    public void uploadUriToDatabase(String stringUri) {
 
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("links");
+
+        myRef.setValue(stringUri);
+
+
+    }
 
 }
